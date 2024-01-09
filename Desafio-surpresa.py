cb90fb5126d3db1bf433187f8ced91414f9110ff
Thiago Ivo <thiagoics16@gmail.com>
2024-01-09 10:56:51 -0300

    Código inicial de cadastro pokemon em Python

---
+++ Desafio-surpresa.py
@@ -0,0 +1,17 @@
+poochyena0 = ["Poochyena", 2, "M", 13, 13]
+zigzagoon0 = ["Zigzagoon", 2, "F", 13, 13]
+dragonite0 = ["Dragonite", 5, "M", 25, 25]
+dragonite1 = ["Dragonite", 5, "F", 24, 24]
+dragonite2 = ["Dragonite", 5, "F", 24, 24]
+poochyena1 = ["Poochyena", 3, "F", 15, 15]
+wurmple0 = ["Wurmple", 2, "M", 7, 14]
+
+pokemon = [poochyena0,zigzagoon0,dragonite0,dragonite1,dragonite2,poochyena1,wurmple0]
+
+print(pokemon[0][0], "cadastrado com sucesso")
+print(pokemon[1][0], "cadastrado com sucesso")
+print(pokemon[2][0], "cadastrado com sucesso")
+print(pokemon[3][0], "cadastrado com sucesso")
+print(pokemon[4][0], "cadastrado com sucesso")
+print(pokemon[5][0], "cadastrado com sucesso")
+print(pokemon[6][0], "cadastrado com sucesso")
